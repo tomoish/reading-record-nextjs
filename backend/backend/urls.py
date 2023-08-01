e1f12dc15a2ca57ae7f9d7210e0bd94601dbfadc
tomoish <tomo.i.120608@gmail.com>
2023-08-01 22:00:57 +0900

    Add urls to login user

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('record.urls')),
     path('api/', include('account.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view()),
 ]
