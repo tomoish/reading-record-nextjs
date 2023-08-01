12aae21304dbcfc2cb402d44a36f66784d403bb0
tomoish <tomo.i.120608@gmail.com>
2023-08-01 19:38:29 +0900

    Add save function for bookBD api

--- backend/record/models.py
+++ backend/record/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import requests
+
+
 class Record(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     book_title = models.CharField(max_length=100)
@@ -14,3 +17,23 @@ class Record(models.Model):
 
     def __str__(self):
         return self.book_title
+
+    def save(self, *args, **kwargs):
+        # openBD api
+        if self.isbn is not None:
+            endpoint = 'https://api.openbd.jp/v1/get'
+            params = {
+                'isbn': self.isbn
+            }
+
+            result = requests.get(endpoint, params=params)
+
+            res = result.json()
+            if res is not None and res[0] is not None:
+                self.thumbnail_url = res[0]['summary']['cover']
+            else:
+                self.thumbnail_url = None
+        else:
+            self.thumbnail_url = None
+
+        super(Record, self).save(*args, **kwargs)
