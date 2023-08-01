f86e459a90a0ec64c17385ea0c3d461e831b293c
tomoish <tomo.i.120608@gmail.com>
2023-08-01 14:31:41 +0900

    Add record model

--- backend/record/models.py
+++ backend/record/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Record(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    book_title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, blank=True, null=True)
+    thumbnail_url = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField()
+    first_page = models.IntegerField()
+    final_page = models.IntegerField()
+    impression = models.TextField(max_length=200)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.book_title
