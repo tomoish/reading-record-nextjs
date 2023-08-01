f148e2b2b397691d315962159d025927c4a6354b
tomoish <tomo.i.120608@gmail.com>
2023-08-01 22:35:38 +0900

    Add function to update user

--- backend/account/views.py
+++ backend/account/views.py
@@ -42,7 +42,27 @@ def register(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def currentUser(request):
-
     user = UserSerializer(request.user)
 
     return Response(user.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request):
+    user = request.user
+
+    data = request.data
+
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
