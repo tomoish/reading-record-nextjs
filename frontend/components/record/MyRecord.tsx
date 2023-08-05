fc7f30ebd83af141e1a21580875c3258bdd19626
tomoish <tomo.i.120608@gmail.com>
2023-08-05 15:28:27 +0900

    Fix MyRecord

--- frontend/components/record/MyRecord.tsx
+++ frontend/components/record/MyRecord.tsx
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect } from "react";
-import Link from "next/link";
-import DataTable from "react-data-table-component";
 import RecordItem from "./RecordItem";
 
 import RecordContext from "@/context/RecordContext";
