7c25e44c51325aec34461585a0c99d549f735f1c
tomoish <tomo.i.120608@gmail.com>
2023-08-08 22:43:25 +0900

    Add AccountType and RecordType

---
+++ frontend/types/AccountType.ts
@@ -0,0 +1,6 @@
+export type AccountType = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+};
---
+++ frontend/types/RecordType.ts
@@ -0,0 +1,11 @@
+export type RecordType = {
+  id: number;
+  book_title: string;
+  isbn: string;
+  thumbnail_url?: string | null;
+  first_page: number;
+  final_page: number;
+  impression: string;
+  posted_at: string;
+  user: number;
+};
