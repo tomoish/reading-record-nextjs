76798962c2dec8dbff37e0c836378416b5d87556
tomoish <tomo.i.120608@gmail.com>
2023-08-10 16:09:12 +0900

    Add NewRecordTests

--- backend/record/tests.py
+++ backend/record/tests.py
@@ -1,3 +1,81 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class NewRecordTests(TestCase):
+
+    def setUp(self):
+        self.user_data = {
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+            'email': 'test@test.com',
+            'password': '123456',
+        }
+
+        self.login_data = {
+            'username': 'test@test.com',
+            'password': '123456',
+        }
+
+        self.record_data = {
+            'book_title': 'Sample book',
+            'isbn': '9784098507177',
+            'date': '2023-08-01',
+            'first_page': '2',
+            'final_page': '100',
+            'impression': 'This book was amazing!'
+        }
+
+    def get_access_token(self):
+        response = Client().post(
+            '/api/register/',
+            data=self.user_data
+        )
+
+        response = Client().post(
+            '/api/token/',
+            data=self.login_data
+        )
+
+        return response.json()['access']
+
+    def test_post_record(self):
+        access_token = self.get_access_token()
+
+        response = Client().post(
+            '/api/records/new/',
+            data=self.record_data,
+            headers={
+                'authorization': 'Bearer ' + access_token
+            },
+            content_type='application/json'
+
+        )
+
+        # print(response)
+        # print(response.json())
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('Sample book', response.json()['book_title'])
+        self.assertEqual('9784098507177', response.json()['isbn'])
+        self.assertEqual('https://cover.openbd.jp/9784098507177.jpg',
+                         response.json()['thumbnail_url'])
+        self.assertEqual('2023-08-01', response.json()['date'])
+        self.assertEqual(2, response.json()['first_page'])
+        self.assertEqual(100, response.json()['final_page'])
+        self.assertEqual('This book was amazing!',
+                         response.json()['impression'])
+
+    def test_invalid_token(self):
+        response = Client().post(
+            '/api/records/new/',
+            data=self.record_data,
+            headers={
+                'authorization': ''
+            },
+            content_type='application/json'
+        )
+
+        self.assertEqual(401, response.status_code)
+        self.assertEqual('Login first to access this resource.',
+                         response.json()['error'])
