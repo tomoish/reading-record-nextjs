bf330bc05a1e8d46c87d287058755cd891daf93c
tomoish <tomo.i.120608@gmail.com>
2023-08-22 23:27:28 +0900

    Fix .env location in django.yml

--- backend/set-up-env.test.sh
+++ backend/set-up-env.test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # DB
-echo "DATABASE_URL=$DATABASE_URL" >> .env
-echo "DATABASE_DB=$DATABASE_DB" >> .env
-echo "DATABASE_USER=$DATABASE_USER" >> .env
-echo "DATABASE_PASSWORD=$DATABASE_PASSWORD" >> .env
-echo "DATABASE_HOST=$DATABASE_HOST" >> .env
-echo "DATABASE_PORT=$DATABASE_PORT" >> .env
+echo "DATABASE_URL=$DATABASE_URL" >> backend/.env
+echo "DATABASE_DB=$DATABASE_DB" >> backend/.env
+echo "DATABASE_USER=$DATABASE_USER" >> backend/.env
+echo "DATABASE_PASSWORD=$DATABASE_PASSWORD" >> backend/.env
+echo "DATABASE_HOST=$DATABASE_HOST" >> backend/.env
+echo "DATABASE_PORT=$DATABASE_PORT" >> backend/.env
 
-echo "POSTGRES_USER=$POSTGRES_USER" >> .env
-echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
-echo "POSTGRES_DB=$POSTGRES_DB" >> .env
+echo "POSTGRES_USER=$POSTGRES_USER" >> backend/.env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> backend/.env
+echo "POSTGRES_DB=$POSTGRES_DB" >> backend/.env
 
 # Django
-echo "SECRET_KEY=$SECRET_KEY" >> .env
-echo "ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS" >> .env
-echo "DEBUG=$DEBUG" >> .env
-echo "CSRF_TRUSTED_ORIGINS=$CSRF_TRUSTED_ORIGINS" >> .env
+echo "SECRET_KEY=$SECRET_KEY" >> backend/.env
+echo "ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS" >> backend/.env
+echo "DEBUG=$DEBUG" >> backend/.env
+echo "CSRF_TRUSTED_ORIGINS=$CSRF_TRUSTED_ORIGINS" >> backend/.env
